da9f1e93783847ca3c92fc50982198379b959065
David Gross <dgross@mimetic.com>
2013-10-01 17:42:06 -0700

    Update header for the WP plugin to a newer style.
    - Update header for plugin to newer style. Not sure it works.
    - Updated the MBA card theme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 mimetic-books-api
 =================
 
-Wordpress plugin to create Mimetic Books books.
+Wordpress plugin to create Mimetic Books book packages and host a bookstore.
+
+This plugin allows WordPress bloggers to publish books using the Mimetic Books publishing system. The plugin handles both the creation of book packages from blog posts and the distribution of book packages to Mimetic Books-based apps.
--- mb-api.php
+++ mb-api.php
@@ -1,11 +1,28 @@
 <?php
 /*
-Plugin Name: Mimetic Books API
-Plugin URI: http://wordpress.org/extend/plugins/mb-api/
-Description: A RESTful API for WordPress eBook Publishing with Mimetic Books
-Version: 0.0.1
-Author: David Gross
-Author URI: http://davidgross.org/
+* Plugin Name: Mimetic Books API
+* Plugin URI: http://wordpress.org/extend/plugins/mb-api/
+* Description: A RESTful API for WordPress eBook Publishing with Mimetic Books
+* Version: 0.1
+* Author: David Gross
+* Author URI: http://davidgross.org/
+* License: GPL2
+*/
+
+/*  Copyright 2013  David Ian Gross  (email : info@mimetic.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 $dir = mb_api_dir();
